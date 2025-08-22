1ce9370b1ebc0c0c59f46cdc11dbc0d105f49294
Jafrul Amin <116928122+jafrulamin@users.noreply.github.com>
2025-08-22 00:12:24 -0400

    Remove night mode toggle functionality

--- main.js
+++ main.js
@@ -1,19 +1,1 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const toggle = document.getElementById("nightToggle");
-    const body = document.body;
 
-    // Check localStorage for saved mode
-    if (localStorage.getItem("nightMode") === "true") {
-        toggle.classList.add("active");
-        body.classList.add("dark");
-    }
-
-    // Toggle the nighmode upon click of button
-    toggle.addEventListener("click", () => {
-        toggle.classList.toggle("active");
-        body.classList.toggle("dark");
-
-        // Save the current mode in localStorage
-        localStorage.setItem("nightMode", body.classList.contains("dark"));
-    });
-})
